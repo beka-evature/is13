7e2b4e5af6211b91fb045e6378d177d1281acc99
Tal Weiss <tal@evature.com>
2015-01-08 08:31:47 +0200

    The code is written but it doesn't work yet (returns 100% errors)

--- data/load_eva.py
+++ data/load_eva.py
@@ -7,6 +7,8 @@ Created on Dec 17, 2014
 
 from __future__ import unicode_literals, division
 from os import listdir
+from os.path import isfile, join
+import os
 import json
 import time
 import sys
@@ -15,16 +17,18 @@ import re
 import numpy as np
 from sklearn.cross_validation import train_test_split
 from collections import Counter
-from os.path import isfile, join
 from core.factory import get_time_parser
 from core.locales import Locales
 from core.infra.tokenizer import tokenize
 import subprocess
 from metrics.accuracy import conlleval
 from utils.tools import shuffle, minibatch, contextwin
-from rnn.elman import model
-import os
+from rnn.elman2vec import model as elman2vec
+from rnn.elman import model as regular_elman
+import theano
+from gensim.models.word2vec import Word2Vec
 
+WORD2VEC_FILENAME = "/home/tal/Downloads/GoogleNews-vectors-negative300.bin"
 DTP = get_time_parser()
 SESSION_PATH = "/home/tal/workspace/sessions"
 LABELS2IDX = {"O":0, "B-when": 1, "I-when": 2, "B-duration": 3, "I-duration": 4}
@@ -57,8 +61,8 @@ def session_to_input(session):
 
 def dtp_search(sentence, expected_meaning):
     """Date time parser search"""
-    return DTP.search(sentence, enable_typos = False, locale = Locales.english_us,
-                      expected_meaning = expected_meaning, from_speech = True, search_within_results = False)
+    return DTP.search(sentence, enable_typos=False, locale=Locales.english_us,
+                      expected_meaning=expected_meaning, from_speech=True, search_within_results=False)
 
 def to_iob(token_list, dtp_search_res):
     """Change DTP search results to IOB"""
@@ -78,7 +82,7 @@ def to_iob(token_list, dtp_search_res):
         elif any(token.start in duration for duration in durations):
             result = "I-duration"
         return result
-    return [token_to_iob(token) for token in token_list ]
+    return [token_to_iob(token) for token in token_list]
 
 def session_to_text0(session):
     """Get the first text utterence in a session"""
@@ -93,11 +97,30 @@ def get_words2idx(session_files):
         sentence = session_to_text0(session)
         token_list = tokenize(sentence)
         for token in token_list:
+            tokens[token.lower()] += 1
             tokens[re.sub(r"\d", "DIGIT", token.lower())] += 1
-    tokens_clean = dict((k, v) for k, v in tokens.iteritems() if v > 1)
+    tokens_clean = dict((k, v) for k, v in tokens.iteritems()) # if v > 1) - words that happen only once -> unknown
     words2idx = dict((k, i) for i, (k, v) in enumerate(tokens_clean.iteritems()))
     words2idx["<UNK>"] = max(index for index in words2idx.itervalues()) + 1
     return words2idx
+
+def get_char_to_idx(session_files):
+    """Get an index for each char"""
+    chars = {c for session_file in session_files for c in session_to_text0(json.loads(open(session_file, "rb").read()))}
+    char2idc = {c: idx for idx, c in enumerate(list(chars))}
+    char2idc["_"] = len(char2idc)
+    return char2idc
+
+def get_session_files(number_of_files=None, random_seed=None):
+    """Get shuffled session files"""
+    _session_files = [join(SESSION_PATH, f) for f in listdir(SESSION_PATH) if f.endswith("json")]
+    session_files = [f for f in _session_files if isfile(f)]
+    if random_seed is not None:
+        random.seed(random_seed)
+    random.shuffle(session_files)
+    if number_of_files:
+        session_files = session_files[:number_of_files]
+    return session_files
 
 def prepare_data():
     """Prepare the data"""
@@ -106,18 +129,14 @@ def prepare_data():
             'verbose': False,
             'decay': True, # decay on the learning rate if improvement stops
             'win': 7, # number of words in the context window
-            'bs': 9, # number of backprop through time steps
+            'bs': 9, # number of back-propagation through time steps
             'nhidden': 100, # number of hidden units
             'seed': 345,
-            'emb_dimension': 100, # dimension of word embedding
+            'emb_dimension': 300, # dimension of word embedding
             'nepochs': 50}
-
-    _session_files = [join(SESSION_PATH, f) for f in listdir(SESSION_PATH) if f.endswith("json")]
-
-    session_files = [f for f in _session_files if isfile(f)]
+    np.random.seed(conf['seed'])
     random.seed(conf['seed'])
-    random.shuffle(session_files)
-    session_files = session_files[:20000] # Limit the span To speed things up...
+    session_files = get_session_files(number_of_files=None, random_seed=conf['seed']) # Limit the scope To speed things up...
     sentences = []
     idxes = []
     labels = []
@@ -136,15 +155,18 @@ def prepare_data():
         dtp_search_res = dtp_search(sentence, None)
         iobes = to_iob(token_list, dtp_search_res)
         labels.append(iobes)
-        labels_idxes.append(np.fromiter((LABELS2IDX[iob] for iob in iobes), dtype = np.int32))
-        token_list = [re.sub(r"\d", "DIGIT", token) for token in token_list]
-        idxes.append(np.fromiter((words2idx.get(token, unknown) for token in token_list), dtype = np.int32))
-    print "Prepare train, validation and test sets"
-    train_valid_lex, test_lex, train_valid_y, test_y = train_test_split(idxes, labels_idxes, test_size = 0.15, random_state = 42)
-    train_lex, valid_lex, train_y, valid_y = train_test_split(train_valid_lex, train_valid_y, test_size = 0.2, random_state = 42)
+        labels_idxes.append(np.fromiter((LABELS2IDX[iob] for iob in iobes), dtype=np.int32))
+#         token_list = [re.sub(r"\d", "DIGIT", token) for token in token_list]
+        idxes.append(np.fromiter((words2idx.get(token, unknown) for token in token_list), dtype=np.int32))
 
-    idx2label = dict((k, v) for v, k in LABELS2IDX.iteritems())
-    idx2word = dict((k, v) for v, k in words2idx.iteritems())
+
+
+    print "Prepare train, validation and test sets"
+    train_valid_lex, test_lex, train_valid_y, test_y = train_test_split(idxes, labels_idxes, test_size=0.15, random_state=42)
+    train_lex, valid_lex, train_y, valid_y = train_test_split(train_valid_lex, train_valid_y, test_size=0.2, random_state=42)
+
+    idx2label = dict((k, v) for v, k in LABELS2IDX.iteritems()) # Reverse the dictionary
+    idx2word = dict((k, v) for v, k in words2idx.iteritems()) # Reverse the dictionary
 
     vocsize = len(idx2word)
 
@@ -154,23 +176,40 @@ def prepare_data():
     folder = os.path.basename(__file__).split('.')[0]
     if not os.path.exists(folder):
         os.mkdir(folder)
-    # instanciate the model
-    np.random.seed(conf['seed'])
-    random.seed(conf['seed'])
-    rnn = model(nh = conf['nhidden'],
-                nc = nclasses,
-                ne = vocsize,
-                de = conf['emb_dimension'],
-                cs = conf['win'])
+
+    print "Loading Word2Vec"
+    word2vec = Word2Vec.load_word2vec_format(WORD2VEC_FILENAME, binary=True) # C binary format
+
+    print "Calculate word embeddings"
+    embeddings = 0.2 * np.random.uniform(-1.0, 1.0, (vocsize + 1, conf['emb_dimension'])).astype(theano.config.floatX) # add one for PADDING at the end @UndefinedVariable
+    for idx, word in idx2word.iteritems():
+        try:
+            embedding = word2vec[word]
+        except KeyError:
+            try:
+                embedding = word2vec[word.capitalize()]
+            except KeyError:
+                embedding = embeddings[idx] # Keep it random
+        embeddings[idx] = embedding
+
+    del word2vec # It is huge
+
+    print "Create a Neural Network"
+    rnn = elman2vec(nh=conf['nhidden'],
+                nc=nclasses,
+                ne=vocsize,
+                de=conf['emb_dimension'],
+                cs=conf['win'],
+                embeddings=embeddings)
 
     # train with early stopping on validation set
     best_f1 = -np.inf
     conf['clr'] = conf['lr']
     print "Start training"
-    for e in xrange(conf['nepochs']):
+    for epoch in xrange(conf['nepochs']):
         # shuffle
         shuffle([train_lex, train_y], conf['seed'])
-        conf['ce'] = e
+        conf['ce'] = epoch
         tic = time.time()
         for i in xrange(nsentences):
             cwords = contextwin(train_lex[i], conf['win'])
@@ -178,9 +217,9 @@ def prepare_data():
             labels = train_y[i]
             for word_batch , label_last_word in zip(words, labels):
                 rnn.train(word_batch, label_last_word, conf['clr'])
-                rnn.normalize()
+#                 rnn.normalize()
             if conf['verbose']:
-                print '[learning] epoch %i >> %2.2f%%' % (e, (i + 1) * 100. / nsentences), 'completed in %.2f (sec) <<\r' % (time.time() - tic),
+                print '[learning] epoch %i >> %2.2f%%' % (epoch, (i + 1) * 100. / nsentences), 'completed in %.2f (sec) <<\r' % (time.time() - tic),
                 sys.stdout.flush()
 
         # evaluation // back into the real world : idx -> words
@@ -203,14 +242,14 @@ def prepare_data():
         if res_valid['f1'] > best_f1:
             rnn.save(folder)
             best_f1 = res_valid['f1']
-            print 'NEW BEST: epoch', e, 'valid F1', res_valid['f1'], 'best test F1', res_test['f1'], ' ' * 20
+            print 'NEW BEST: epoch', epoch, 'valid F1', res_valid['f1'], 'best test F1', res_test['f1'], ' ' * 20
             conf['vf1'], conf['vp'], conf['vr'] = res_valid['f1'], res_valid['p'], res_valid['r']
             conf['tf1'], conf['tp'], conf['tr'] = res_test['f1'], res_test['p'], res_test['r']
-            conf['be'] = e
+            conf['be'] = epoch
             subprocess.call(['mv', folder + '/current.test.txt', folder + '/best.test.txt'])
             subprocess.call(['mv', folder + '/current.valid.txt', folder + '/best.valid.txt'])
         else:
-            print '        : epoch', e, 'valid F1', res_valid['f1'], '     test F1', res_test['f1'], ' ' * 20
+            print '        : epoch', epoch, 'valid F1', res_valid['f1'], '     test F1', res_test['f1'], ' ' * 20
 
         # learning rate decay if no improvement in 10 epochs
         if conf['decay'] and abs(conf['be'] - conf['ce']) >= 10:
@@ -218,9 +257,139 @@ def prepare_data():
         if conf['clr'] < 1e-5:
             break
 
-    print 'BEST RESULT: epoch', e, 'valid F1', res_valid['f1'], 'best test F1', res_test['f1'], 'with the model', folder
+    print 'BEST RESULT: epoch', epoch, 'valid F1', res_valid['f1'], 'best test F1', res_test['f1'], 'with the model', folder
+
+def play_with_splitting_sentences():
+    """Play with splitting sentences"""
+    conf = { # 'fold': 3, # 5 folds 0,1,2,3,4
+        'lr': 0.0627142536696559,
+        'verbose': False,
+        'decay': True, # decay on the learning rate if improvement stops
+        'win': 13, # number of characters in the context window
+        'bs': 4, # number of back-propagation through time steps
+        'nhidden': 10, # number of hidden units
+        'seed': 345,
+        'emb_dimension': 2, # dimension of character embedding
+        'nepochs': 50}
+    np.random.seed(conf['seed'])
+    random.seed(conf['seed'])
+    print "Calculate output"
+    session_files = get_session_files(number_of_files=1000, random_seed=conf['seed']) # Limit the scope To speed things up...
+    labels2idx = {"_": 0, "X": 1}
+    sentences = []
+    idxes = []
+    labels_idxes = []
+    labels = []
+    char2idx = get_char_to_idx(session_files)
+    for session_file in session_files:
+        session = json.loads(open(session_file, "rb").read())
+        sentence = session_to_text0(session)
+        if not sentence.strip():
+            continue
+        sentence_out, label = create_test(sentence, probability=0.2)
+        sentences.append(sentence_out)
+        labels.append(label)
+        labels_idxes.append(np.fromiter((labels2idx[l] for l in label), dtype=np.uint32))
+        idxes.append(np.fromiter((char2idx[char] for char in sentence_out), dtype=np.uint32))
+
+    print "Prepare train, validation and test sets"
+    train_valid_lex, test_lex, train_valid_y, test_y = train_test_split(idxes, labels_idxes, test_size=0.15, random_state=42)
+    train_lex, valid_lex, train_y, valid_y = train_test_split(train_valid_lex, train_valid_y, test_size=0.2, random_state=42)
+
+    idx2label = dict((k, v) for v, k in labels2idx.iteritems()) # Reverse the dictionary
+    idx2word = dict((k, v) for v, k in char2idx.iteritems()) # Reverse the dictionary
+
+    vocsize = len(set(reduce(\
+                       lambda x, y: list(x)+list(y),\
+                       train_lex+valid_lex+test_lex)))
+
+    nclasses = len(set(reduce(lambda x, y: list(x) + list(y), train_y + test_y + valid_y)))
+
+    nsentences = len(train_lex)
+    folder = os.path.basename(__file__).split('.')[0] + "_2"
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    print "Create a Neural Network"
+    rnn = regular_elman(nh=conf['nhidden'],
+                        nc=nclasses,
+                        ne=vocsize,
+                        de=conf['emb_dimension'],
+                        cs=conf['win'],)
+
+    # train with early stopping on validation set
+    best_f1 = -np.inf
+    conf['clr'] = conf['lr']
+    print "Start training"
+    for epoch in xrange(conf['nepochs']):
+        # shuffle
+        shuffle([train_lex, train_y], conf['seed'])
+        conf['ce'] = epoch
+        tic = time.time()
+        for i in xrange(nsentences):
+            cwords = contextwin(train_lex[i], conf['win'])
+            words = [np.asarray(x).astype(np.int32) for x in minibatch(cwords, conf['bs'])]
+            labels = train_y[i]
+            for word_batch , label_last_word in zip(words, labels):
+                rnn.train(word_batch, label_last_word, conf['clr'])
+                rnn.normalize()
+            if conf['verbose']:
+                print '[learning] epoch %i >> %2.2f%%' % (epoch, (i + 1) * 100. / nsentences), 'completed in %.2f (sec) <<\r' % (time.time() - tic),
+                sys.stdout.flush()
+
+        # evaluation // back into the real world : idx -> words
+        predictions_test = [ map(lambda x: idx2label[x], \
+                         rnn.classify(np.asarray(contextwin(x, conf['win'])).astype('int32')))\
+                         for x in test_lex ]
+        groundtruth_test = [ map(lambda x: idx2label[x], y) for y in test_y ]
+        words_test = [ map(lambda x: idx2word[x], w) for w in test_lex]
+
+        predictions_valid = [ map(lambda x: idx2label[x], \
+                             rnn.classify(np.asarray(contextwin(x, conf['win'])).astype('int32')))\
+                             for x in valid_lex ]
+        groundtruth_valid = [ map(lambda x: idx2label[x], y) for y in valid_y ]
+        words_valid = [ map(lambda x: idx2word[x], w) for w in valid_lex]
+
+        # evaluation // compute the accuracy using conlleval.pl
+        res_test = conlleval(predictions_test, groundtruth_test, words_test, folder + '/current.test.txt')
+        res_valid = conlleval(predictions_valid, groundtruth_valid, words_valid, folder + '/current.valid.txt')
+
+        if res_valid['f1'] > best_f1:
+            rnn.save(folder)
+            best_f1 = res_valid['f1']
+            print 'NEW BEST: epoch', epoch, 'valid F1', res_valid['f1'], 'best test F1', res_test['f1'], ' ' * 20
+            conf['vf1'], conf['vp'], conf['vr'] = res_valid['f1'], res_valid['p'], res_valid['r']
+            conf['tf1'], conf['tp'], conf['tr'] = res_test['f1'], res_test['p'], res_test['r']
+            conf['be'] = epoch
+            subprocess.call(['mv', folder + '/current.test.txt', folder + '/best.test.txt'])
+            subprocess.call(['mv', folder + '/current.valid.txt', folder + '/best.valid.txt'])
+        else:
+            print '        : epoch', epoch, 'valid F1', res_valid['f1'], '     test F1', res_test['f1'], ' ' * 20
+
+        # learning rate decay if no improvement in 10 epochs
+        if conf['decay'] and abs(conf['be'] - conf['ce']) >= 10:
+            conf['clr'] *= 0.5
+        if conf['clr'] < 1e-5:
+            break
+
+    print 'BEST RESULT: epoch', epoch, 'valid F1', res_valid['f1'], 'best test F1', res_test['f1'], 'with the model', folder
 
 
+def create_test(sentence, probability):
+    """Concatenate some words"""
+    words = sentence.split()
+    if words:
+        output, labels = words[0], "_" * len(words[0])
+        for word in words[1:]:
+            if random.random() > probability:
+                output += "_" + word
+                labels += "_" * (len(word) + 1)
+            else:
+                output += word
+                labels += "X" + "_" * (len(word) - 1)
+    else:
+        output = labels = ""
+    return output, labels
 
 if __name__ == '__main__':
-    prepare_data()
+#     prepare_data()
+    play_with_splitting_sentences()
